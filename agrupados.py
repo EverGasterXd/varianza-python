cd260204e39d1a316d079886f9468b5bc565cb9a
EverGaster <98375412+EverGasterXd@users.noreply.github.com>
2024-05-14 21:44:19 -0600

    Revert "arreglos"
    This reverts commit f911d15fde93395247c10d7dcb8de5145f20db54.

--- agrupados.py
+++ agrupados.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from tabulate import tabulate
 
 class CrearTabla:
     def __init__(self, datos):
@@ -9,59 +8,56 @@ class CrearTabla:
     def calcular_tabla(self, num_clases=None):
         if num_clases is None:
             # Calcular el número de clases utilizando la regla de Scott
-            ancho_banda = 3.5 * self.std() / (self.num_datos ** (1/3))
-            num_clases = int((max(self.datos) - min(self.datos)) / ancho_banda)
+            ancho_banda = 3.5 * self.datos['xi'].std() / (self.num_datos ** (1/2))
+            num_clases = int((max(self.datos['xi']) - min(self.datos['xi'])) / ancho_banda)
         
         # Calcular los límites de clase
         límites_clase = self.calcular_límites_clase(num_clases)
         
-        # Calcular las frecuencias
-        frecuencias = self.calcular_frecuencias(límites_clase)
+        # Calcular las frecuencias absolutas
+        frecuencias_absolutas = pd.cut(self.datos['xi'], límites_clase, include_lowest=True, right=False).value_counts().sort_index().tolist()
         
         # Calcular frecuencia relativa
-        frecuencia_relativa = frecuencias / self.num_datos
+        frecuencia_relativa = [round(f / self.num_datos, 1) for f in frecuencias_absolutas]
         
         # Calcular frecuencia acumulada
-        frecuencia_acumulada = frecuencias.cumsum()
+        frecuencia_acumulada = pd.Series(frecuencias_absolutas).cumsum().tolist()
         
         # Calcular frecuencia relativa acumulada
-        frecuencia_relativa_acumulada = frecuencia_relativa.cumsum()
+        frecuencia_relativa_acumulada = pd.Series(frecuencia_relativa).cumsum().tolist()
         
         # Calcular el punto medio de cada clase
         puntos_medios = self.calcular_puntos_medios(límites_clase)
         
         # Crear DataFrame para la tabla de frecuencia
         tabla = pd.DataFrame({
-            'Límite inferior': límites_clase[:-1],
-            'Límite superior': límites_clase[1:],
-            'Punto medio': puntos_medios,
-            'Frecuencia absoluta': frecuencias,
+            'Clases': [f"{round(límites_clase[i], 1)} - {round(límites_clase[i+1], 1)}" for i in range(len(límites_clase) - 1)],
+            'xi': [round(x, 1) for x in puntos_medios],
+            'Frecuencia absoluta': frecuencias_absolutas,
             'Frecuencia relativa': frecuencia_relativa,
             'Frecuencia acumulada': frecuencia_acumulada,
             'Frecuencia relativa acumulada': frecuencia_relativa_acumulada
         })
         
         return tabla
-    
-    def std(self):
-        mean = sum(self.datos) / self.num_datos
-        variance = sum((x - mean) ** 2 for x in self.datos) / self.num_datos
-        return variance ** 0.5
-    
-    def calcular_límites_clase(self, num_clases):
-        min_dato = min(self.datos)
-        max_dato = max(self.datos)
-        ancho_clase = (max_dato - min_dato) / num_clases
-        return [min_dato + i * ancho_clase for i in range(num_clases + 1)]
 
-    def calcular_frecuencias(self, límites_clase):
-        frecuencias = [0] * (len(límites_clase) - 1)
-        for dato in self.datos:
-            for i, límite in enumerate(límites_clase[:-1]):
-                if dato >= límite and dato < límites_clase[i+1]:
-                    frecuencias[i] += 1
-                    break
-        return frecuencias
+    def calcular_media(self):
+        media = round(sum(self.datos['xi']) / self.num_datos, 1)
+        return media
+
+    def calcular_varianza(self):
+        media = self.calcular_media()
+        suma = sum((xi - media) ** 2 for xi in self.datos['xi'])
+        varianza = round(suma / self.num_datos, 1)
+        return varianza
+
+    def calcular_límites_clase(self, num_clases):
+        epsilon = 1e-10  # Un pequeño valor para ajustar los límites de las clases
+        min_dato = min(self.datos['xi']) - epsilon
+        max_dato = max(self.datos['xi']) + epsilon
+        ancho_clase = (max_dato - min_dato) / num_clases
+        límites_clase = [min_dato + i * ancho_clase for i in range(num_clases + 1)]
+        return límites_clase
 
     def calcular_puntos_medios(self, límites_clase):
         return [(límites_clase[i] + límites_clase[i+1]) / 2 for i in range(len(límites_clase) - 1)]
--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import math
 import agrupados
 import noAgrupados
 from tabulate import tabulate
-
+import os
 datos = input("Ingresa números separados por espacios:\n")
 numeros = [int(d) for d in datos.split() if d.isdigit()]
     
@@ -26,15 +26,53 @@ if len(numeros) <= 29:
         mediana: {noAgrupados.mediana(numeros)}
         moda: {noAgrupados.moda(numeros)}
         desviacion estandar: {noAgrupados.d_e(numeros)}""")
+
+        guardar = input("¿Quieres guardar esta información en un archivo de texto? (s/n): ")
+        if guardar.lower() == 's':
+            with open('resultados.txt', 'w') as f:
+                f.write(tabulate(no_agrupados, headers=headers, tablefmt="psq1"))
+                f.write(f"""
+                {"Los numeros ingresados son:", ", ".join(map(str, numeros))}
+                rango: {rango}
+                varianza: {noAgrupados.varianza(numeros)}
+                media: {noAgrupados.media(numeros)}
+                mediana: {noAgrupados.mediana(numeros)}
+                moda: {noAgrupados.moda(numeros)}
+                """)
+            print("La información se ha guardado en 'resultados.txt'")
+        else:
+             print("cerrando programa")
+             os.system('cls')
+             exit()
+             
 else:
     tabla = agrupados.CrearTabla(numeros)
 
     tabla_frecuencia = tabla.calcular_tabla()
+    varianza = tabla.calcular_varianza()
+    media = tabla.calcular_media()
+    print(tabulate(tabla_frecuencia, headers=headers, tablefmt='psq1'))
+    print(f"""
+    {"Los números ingresados son:", ", ".join(map(str, numeros))}
+    varianza: {tabla.calcular_varianza()}
+    media: {tabla.calcular_media()}
+    mediana: aun no lo hago
+    """)
+    guardar = input("¿Quieres guardar esta información en un archivo de texto? (s/n): ")
+    if guardar.lower() == 's':
+        with open('resultados.txt', 'w') as f:
+            f.write(tabulate(tabla_frecuencia, headers=headers, tablefmt='psq1'))
+            f.write(f"""
+            {"Los números ingresados son:", ", ".join(map(str, numeros))}
+            varianza: {tabla.calcular_varianza()}
+            media: {tabla.calcular_media()}
+            mediana: aun no lo hago
+            """)
+        print("La información se ha guardado en 'resultados.txt'")
+    else:
+        print("cerrando programa")
+        os.system('cls')
+        exit()
 
-    print(tabulate(tabla_frecuencia, headers='key', tablefmt='grid'))
-        
-
-
-
-
+os.system('cls')
 input("Presione Enter para cerrar el programa.")
